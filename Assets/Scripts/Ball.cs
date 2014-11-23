52a8be5c6bab6c620cb3d13c2e22d88113018092
Joseph Constantakis <jcon5294@gmail.com>
2014-11-23 17:39:44 -0500

    create balls

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Ball : MonoBehaviour
+{
+    void Start() {
+    
+    }
+    
+    void Update() {
+    
+    }
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Manager : MonoBehaviour
+{
+    // -- CONSTANTS --
+    const int numBalls = 3;
+
+    private Dictionary<int, Color> colorOfBall =
+        new Dictionary<int, Color>
+    {
+        {0, Color.red},
+        {1, Color.green},
+        {2, Color.white}
+    };
+
+    // -- VARIABLES --
+    public GameObject ground;
+    public List<Ball> balls;
+
+    void Start()
+    {
+        CreateBalls();
+    }
+
+    void CreateBalls()
+    {
+        float groundRadius = ground.transform.localScale.x / 2f;
+        balls = Enumerable.Range(0, numBalls)
+                          .Select(i => {
+            float rad = ((float)i)/((float)numBalls) * 2f*Mathf.PI;
+            Vector3 pos = new Vector3(Mathf.Sin(rad), 0, Mathf.Cos(rad)) * (groundRadius-1);
+            return CreateBall(pos, i);
+        })
+        .ToList();
+    }
+
+    Ball CreateBall(Vector3 pos, int index)
+    {
+        GameObject go = Instantiate(Resources.Load("Ball"), pos, Quaternion.identity) as GameObject;
+        go.renderer.material.color = colorOfBall[index];
+        Ball ball = go.GetComponent<Ball>();
+        return ball;
+    }
+}
